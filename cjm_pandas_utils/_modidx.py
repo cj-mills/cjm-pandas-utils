12fc0ad8f5277443b57af8a4cd4b8295d3a63cb3
cj-mills <millscj.mills2@gmail.com>
2023-02-28 15:35:14 -0800

    Add display_df function.

--- cjm_pandas_utils/_modidx.py
+++ cjm_pandas_utils/_modidx.py
@@ -5,5 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://cj-mills.github.io',
                 'git_url': 'https://github.com/cj-mills/cjm-pandas-utils',
                 'lib_path': 'cjm_pandas_utils'},
-  'syms': { 'cjm_pandas_utils.core': { 'cjm_pandas_utils.core.markdown_to_pandas': ( 'core.html#markdown_to_pandas',
+  'syms': { 'cjm_pandas_utils.core': { 'cjm_pandas_utils.core.display_df': ('core.html#display_df', 'cjm_pandas_utils/core.py'),
+                                       'cjm_pandas_utils.core.markdown_to_pandas': ( 'core.html#markdown_to_pandas',
                                                                                      'cjm_pandas_utils/core.py')}}}
